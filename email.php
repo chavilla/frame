1713be6f23f5cb7772a0050566557cc997cb7bf8
chavilla <jcharris.villa@gmail.com>
2020-06-22 17:33:52 -0500

    Cambios

--- email.php
+++ email.php
@@ -32,16 +32,20 @@ $mail = new PHPMailer();
 try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'ssl://smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->isSMTP();     
+    $mail->smtpConnect([
+        'ssl' => [
+             'verify_peer' => false,
+             'verify_peer_name' => false,
+             'allow_self_signed' => true
+         ]
+     ]);                                       // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'jcharris.villa@gmail.com';                     // SMTP username
     $mail->Password   = 'Chavilla.1993';                               // SMTP password
-    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;
-    $mail->SMTPKeepAlive = true;   
-    $mail->Mailer = "smtp"; // don't change the quotes!                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
     //Recipients
     $mail->setFrom('jcharris.villa@gmail.com', 'Jesu');
     $mail->addAddress('jcharris.villa@gmail.com', 'Jesús');     // Add a recipient
