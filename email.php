4434812a59b83821d54accc283173db8d8d47f60
chavilla <jcharris.villa@gmail.com>
2020-06-22 17:20:05 -0500

    Cambios

--- email.php
+++ email.php
@@ -31,13 +31,13 @@ $mail = new PHPMailer();
 
 try {
     //Server settings
-    $mail->SMTPDebug = 2;//SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->Host       = 'ssl://smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'jcharris.villa@gmail.com';                     // SMTP username
     $mail->Password   = 'Chavilla.1993';                               // SMTP password
-    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
