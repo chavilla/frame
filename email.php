a44cd301e4155f7575d011391ec18e5cb22086e4
chavilla <jcharris.villa@gmail.com>
2020-06-22 17:14:09 -0500

    Cambios

--- email.php
+++ email.php
@@ -31,7 +31,7 @@ $mail = new PHPMailer();
 
 try {
     //Server settings
-    $mail->SMTPDebug = 0;//SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->SMTPDebug = 2;//SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
