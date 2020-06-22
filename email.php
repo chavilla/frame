ed4da7437fa86724009c1b81d922bfca0cdf84a6
chavilla <jcharris.villa@gmail.com>
2020-06-22 17:22:31 -0500

    Cambios

--- email.php
+++ email.php
@@ -37,7 +37,7 @@ try {
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'jcharris.villa@gmail.com';                     // SMTP username
     $mail->Password   = 'Chavilla.1993';                               // SMTP password
-    $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
